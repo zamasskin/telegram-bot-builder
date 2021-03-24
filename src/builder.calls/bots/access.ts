973a7b9641b68dfdfdf5f0a2683f1dbd93cb90c7
Alexey Zamasskin <zamasskin@yandex.ru>
2021-03-24 14:18:51 +0300

    Добавил функцию удаления доступов

---
+++ src/builder.calls/bots/access.ts
@@ -0,0 +1,50 @@
+import TelegramBot, {Message} from 'node-telegram-bot-api';
+import {SettingsEntity} from '../../entities/settings.entity';
+import {getRepository} from 'typeorm';
+import {addAccess, removeAccess} from '../../access';
+import {callCommand} from '../../bot';
+import {CommandsEntity} from '../../entities/commands.entity';
+
+export async function addBotAccess(
+  bot: TelegramBot,
+  msg: Message,
+  settings: SettingsEntity
+) {
+  const chatId = msg.chat.id;
+  if (!msg.text) {
+    await bot.sendMessage(chatId, 'Введите корректное имя пользователя');
+    return;
+  }
+  await addAccess(settings.bot, msg.text, 'W');
+  const command = await getRepository(CommandsEntity).findOne({
+    where: {id: 7},
+    relations: ['bot', 'menu'],
+  });
+  if (command) {
+    await callCommand(bot, msg, settings, command);
+  }
+}
+
+export async function removeBotAccess(
+  bot: TelegramBot,
+  msg: Message,
+  settings: SettingsEntity
+) {
+  const chatId = msg.chat.id;
+  if (!msg.text) {
+    await bot.sendMessage(chatId, 'Введите корректное имя пользователя');
+    return;
+  }
+
+  if (await removeAccess(settings.bot, msg.text)) {
+    const command = await getRepository(CommandsEntity).findOne({
+      where: {id: 7},
+      relations: ['bot', 'menu'],
+    });
+    if (command) {
+      await callCommand(bot, msg, settings, command);
+    }
+  } else {
+    await bot.sendMessage(chatId, 'Доступ не найден');
+  }
+}
